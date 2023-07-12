edf8bd233683ad8a6a6e4a8797e807c81164ad97
KAJIHARA SOMA <n23008@std.it-college.ac.jp>
2023-07-12 06:41:50 +0000

    syame

--- CHAPTER02/Q2_9_1.py
+++ CHAPTER02/Q2_9_1.py
@@ -1,6 +1,3 @@
-x = True
-y = False
-z = None
-print(x and z is None)
-print(not x or not y)
-print(x and not y and z is None)
+print("apple" in ["pineapple", "orange", "banana"])
+
+print("apple" in "pineapple")
