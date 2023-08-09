40edd30fee50f47f48c40cc0d74f3f1e33d91111
KAJIHARA SOMA <n23008@std.it-college.ac.jp>
2023-08-09 05:10:44 +0000

    syame

--- CHAPTER04/Q4_8_1.py
+++ CHAPTER04/Q4_8_1.py
@@ -1,9 +1,21 @@
+import time
+
+
 def show_begin_end(func):
-       呼ばれた関数の始めと終わりを表示するデコレータ
     def deco_func(*args, **kwargs):
-           関数を実行する前を後にメッセージを表示
-        print('== stsrt')
+        print("== start")
         result = func(*args, **kwargs)
-        print('== end')
-        return result
+        result
+        print("== end")
+
     return deco_func
+
+
+@show_begin_end
+def sleep_for_a_while():
+    print("Sleeping")
+    time.sleep(2)
+    print("Done Sleeping")
+
+
+sleep_for_a_while()
