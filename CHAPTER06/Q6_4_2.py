18257bdb20c4c3d6692814cc44820ddf45a044b5
KAJIHARA SOMA <n23008@std.it-college.ac.jp>
2023-08-30 19:21:49 +0000

    syame

--- CHAPTER06/Q6_4_2.py
+++ CHAPTER06/Q6_4_2.py
@@ -1,32 +1,17 @@
 import math
 
 
-def gen_prime1(x=2):
+def gen_prime(x=2):
     while True:
         for i in range(2, int(math.sqrt(x)) + 1):
             if x % i == 0:
                 break
         else:
             yield x
-        x += 1
+            x += 1
 
 
-i = gen_prime()
-for c in range(10):
-    print(next(i), end=" ")
-print("")
-
-% % time
-# (1)愚直な方法
-i = gen_prime(100000)
-for c in range(10):
-    print(next(i), end=" ")
-print("")
-
-
-% % time
-# (2)sqrt(x)以下だけ調べる方法
-i = gen_prime1(100000)
+i = gen_prime(10000)
 for c in range(10):
     print(next(i), end=" ")
 print("")
